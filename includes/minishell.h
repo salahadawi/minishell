9361017067d4a5fabf8c24677f3c6a47b3bf3183
sadawi <hahli144@hotmail.com>
2020-04-17 13:32:07 +0300

    add function prototypes, name typedef according to norm

--- includes/minishell.h
+++ includes/minishell.h
@@ -37,4 +37,88 @@ typedef struct		s_env
 	t_builtin_func	**builtin_funcs;
 }					t_env;
 
+void				print_error(char *message);
+
+int					count_env_amount(char *envp[]);
+
+char				*ft_strsubchar(char const *s, size_t start, char c);
+
+int					builtin_echo(t_env *env, char **args);
+
+int					builtin_exit(t_env *env, char **args);
+
+int					builtin_env(t_env *env, char **args);
+
+int					check_names_match(char *var1, char *var2);
+
+void				add_env(t_env *env, char *arg);
+
+int					builtin_setenv(t_env *env, char **args);
+
+int					shortcut_setenv(t_env *env, char **args);
+
+int					update_env(t_env *env, char **new_envp, int deleted, int i);
+
+int					delete_env(t_env *env, char *arg);
+
+int					builtin_unsetenv(t_env *env, char **args);
+
+char				*strsub_alphanumeric_underscore(char *str);
+
+char				*get_env_value(t_env *env, char *name);
+
+char				*store_oldpwd(t_env *env);
+
+void				update_oldpwd(t_env *env, char *path);
+
+void				update_pwd(t_env *env);
+
+int					shortcut_cd(t_env *env, char **args);
+
+int					builtin_cd(t_env *env, char **args);
+
+void				init_builtins(t_env *env);
+
+void				init_env(t_env **env, char *envp[]);
+
+void				print_envs(t_env *env);
+
+char				**split_line_args(char *line);
+
+char				*find_filepath(t_env *env, char *filename);
+
+void				wait_for_child(pid_t pid);
+
+int					exec_cmd(t_env *env, char **args);
+
+int					find_size_pointers(char *str, char *ptr);
+
+int					get_env_name_len(char *str);
+
+char				*expand_tilde(t_env *env, char *str, char *ptr);
+
+char				*expand_dollar(t_env *env, char *str, char *ptr);
+
+char				*find_dollar(const char *s);
+
+void				handle_expansion(t_env *env, char **args);
+
+int					handle_shortcuts(t_env *env, char **args);
+
+int					handle_builtins(t_env *env, char **args);
+
+int					check_cmd(t_env *env, char **args);
+
+void				print_current_dir_basename(void);
+
+void				free_args(char **args);
+
+char				**split_line_commands(char *line);
+
+void				print_shell_info(t_env *env);
+
+void				loop_shell(t_env *env);
+
+void				clear_screen(t_env *env);
+
 #endif
